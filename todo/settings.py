f47ac782df78140acc027f303ac9fbd51231ce33
Aniket Raj <aniketraj2014@gmail.com>
2020-05-06 22:51:50 +0530

    required commit

--- todo/settings.py
+++ todo/settings.py
@@ -123,3 +123,10 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 LOGIN_URL = '/login/'
+
+
+
+try:
+    from .local_settings import *
+except ImportError:
+    print('you are on production')
